a72d5fbebfe91645c41b23cf16df0e9f3ae7185d
GilbertHLam <gilberthlam@cmail.carleton.ca>
2017-12-17 00:55:14 -0500

    Set up database with info, and started API Added initialData.xml - An XML file used to store the initial database information
    Added addToDatabase.js - Reads the XML file and adds it to the SQLite DB
    
    Modified app.js - Started GET Requests

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,77 @@
-const MongoClient = require('mongodb').MongoClient
 
-const DB_PATH = 'mongodb://bb_admin:veryverysecure@ds129166.mlab.com:29166/bathroom_banter'
+// ==================================================================================
+// Declaration of constant variables and imports of node modules
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
+const http = require('http');
+const express = require('express');
+const PORT = process.env.PORT || 3000
+const app = express();
+
+let db = new sqlite3.Database('./washrooms.db',sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the SQLite database.');
+});
+
+// ==================================================================================
 
 
-MongoClient.connect(DB_PATH, function(err, db){
-  if(err) console.log(`FAILED TO CONNECTED TO: ${DB_PATH}`);
-  else{
-    console.log(`CONNECTED TO: ${DB_PATH}`);
-    var collection = db.collection('data');
-    var cursor = collection.find();
-    collection.find({buildings : { name : "Herzberg Laboratories" }}).toArray(function (err, docs) {
+// ==================================================================================
+// Middleware declaration
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// ==================================================================================
 
-      if(err) throw err;
 
-      docs.forEach(function (doc) {
-        console.log(doc);
-      });
+// ==================================================================================
+// API GET Requests
+
+//Gets a list for all the buildings
+app.get('/listOfBuildings', (request, response) => {
+  var returnObject = {};
+  var listOfBuildings = [];
+  var sqlQuery = 'SELECT building FROM washrooms;';
+  db.all(sqlQuery,[],(err,rows)=>{
+    if(err)
+      throw err;
+    rows.forEach((element)=>{
+      if(listOfBuildings.indexOf(element.building) == -1){
+        console.log('Building: ' + element.building);
+        listOfBuildings.push(element.building);
+      }
     });
+  });
+  returnObject.listOfBuildings = listOfBuildings;
+  return(returnObject);
+});
 
+// Gets a list of floors for a building
+// Example Request: http://localhost:3000/listOfFloors?building=Herzberg+Laboratories
+app.get('/listOfFloors', (request, response) => {
+  var returnObject = {};
+  var listOfFloors = [];
+  var sqlQuery = 'SELECT floor FROM washrooms WHERE building LIKE "%' + request.query.building + '%";';
+  db.all(sqlQuery,[],(err,rows)=>{
+    if(err)
+      throw err;
+    rows.forEach((element)=>{
+      if(listOfFloors.indexOf(element.floor) == -1){
+        console.log(request.query.building + ' has Floor: ' + element.floor);
+        listOfFloors.push(element.floor);
+      }
+    });
+  });
+  returnObject.listOfFloors = listOfFloors;
+  return(returnObject);
+});
+
+// ==================================================================================
+
+app.listen(PORT, err => {
+  if(err) console.log(err)
+  else {
+    console.log(`Server listening on port: ${PORT}`)
   }
 });
